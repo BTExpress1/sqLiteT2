3eaa62dfd987d0282e18f66381b6aa3ac3d02d8e
Bini Teklehaimanot <padgenb@gmail.com>
2025-02-03 22:33:57 +0000

    Minor Updated to show where solutions are located

--- SQLFiles_Tier2/SQLTasks Tier 2.sql
+++ SQLFiles_Tier2/SQLTasks Tier 2.sql
@@ -155,11 +155,11 @@ for the following questions.  */
 /* QUESTIONS: 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
-that there's a different cost for guests and members! */
+that there's a different cost for guests and members! 
 
 The answers to questions in part 2 are part of the uploaded Jupyter Notebook: 
 
-https://github.com/BTExpress1/sqLiteT2/blob/fbf02331e66872d454bfe7c3625c7f2a41bd2fa5/SQLFiles_Tier2/sql_case_study_t2.ipynb
+https://github.com/BTExpress1/sqLiteT2/blob/fbf02331e66872d454bfe7c3625c7f2a41bd2fa5/SQLFiles_Tier2/sql_case_study_t2.ipynb */
 
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
